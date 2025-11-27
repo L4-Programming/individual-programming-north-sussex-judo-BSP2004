b60c85c74d9c054dfc5da200c546b7977206e642
BSP2004 <pereirabernardo718@gmail.com>
2025-11-27 16:26:50 +0000

    added the basic form validation

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 /* Refer to the README.md for instructions on what you need to do in this project */
 
-alert("Hello, World!");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.querySelector("#athlete-name").value;
+  if (name === "") {
+    alert("Please enter the athlete's name.");
+  }
+});
